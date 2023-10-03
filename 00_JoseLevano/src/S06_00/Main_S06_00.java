ce66fdd7fa7d26cb4a9dd24239d95531fcae7611
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2023-10-03 10:29:21 -0500

    S06

---
+++ 00_JoseLevano/src/S06_00/Main_S06_00.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package S06_00;
+
+/**
+ *
+ * @author developer
+ */
+public class Main_S06_00 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
